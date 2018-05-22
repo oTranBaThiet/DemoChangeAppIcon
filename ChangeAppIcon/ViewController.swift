42987604aa74501dd278a5fc9b8328a52ec0b70a
oTranBaThiet <tran.ba.thiet@framgia.com>
2018-05-22 11:29:24 +0700

    Initial Commit

---
+++ ChangeAppIcon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChangeAppIcon
+//
+//  Created by TranBa Thiet on 5/22/18.
+//  Copyright © 2018 TranBa Thiet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
